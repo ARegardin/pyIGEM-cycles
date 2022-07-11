944893c6358377c36ee5801be5c493ee92560b20
TitouanCH <titouan.azert@wadsa.com>
2022-07-11 19:42:54 +0200

    added micha

--- reaction.py
+++ reaction.py
@@ -1,11 +1,13 @@
 import math
 
 class Reaction:
-    def __init__(self):
+    def __init__(self, t = "linear"):
         self.reagents = []
         self.products = []
-        self.type = "linear"
+        self.type = t
+
         self.constants = []
+        self.speed_func = lambda x : x
         
     def add_reagent(self, reagent, cst):
         self.reagents.append(reagent)
@@ -15,9 +17,23 @@ class Reaction:
         self.products.append(product)
         self.constants.append(cst)
 
-    def react(self, dt):
-        if self.type == "linear":
-            self.react_linear(dt)
+    def react(self, dt, step = 1):
+        for i in range(0, dt, step):
+
+            if self.type == "linear":
+                self.react_linear(step)
+
+            if self.type == "michaelis":
+                self.react_michaelis(step)
+            
+            # Check if reagent are at zero or negative quantity
+            reagent_at_zero = False
+            for j in range(len(self.reagents)):
+                if self.reagents[j].quant <= 0:
+                    reagent_at_zero = True
+                    self.reagents[j].quant = 0
+            if reagent_at_zero:
+                break
 
     def react_linear(self, dt):
         mod = math.floor(len(self.constants)/2)
@@ -25,3 +41,19 @@ class Reaction:
             self.reagents[i].quant -= dt * self.constants[mod - 1 - i]
         for i in range(len(self.products)):
             self.products[i].quant += dt * self.constants[mod + i]
+
+    def react_michaelis(self, dt):
+        mod = math.floor(len(self.constants)/2)
+        speed = abs(self.speed_func(self.reagents[0].quant))
+        
+        for i in range(len(self.reagents)):
+            self.reagents[i].quant -= speed * dt * self.constants[mod - 1 - i]
+        for i in range(len(self.products)):
+            self.products[i].quant += speed * dt * self.constants[mod + i]
+        
+        for i in range(len(self.reagents)):
+            if self.reagents[i].quant < 0:
+                for j in range(len(self.products)):
+                    self.products[j].quant += self.reagents[i].quant * self.constants[mod + i] / self.constants[mod - 1 - j]
+                self.reagents[i].quant = 0
+
