7945f1bffbcb3eaedd8bf1328b18441d9752e8da
Antoine Regardin <antoine.regardin@orange.fr>
2022-07-11 20:08:08 +0200

    update: nom

--- molecule.py
+++ molecule.py
@@ -1,7 +1,8 @@
 #CLASS
 
 class Molecule :
-    def __init__(self, e, quant, disp):
+    def __init__(self, e=0, quant=0, disp=0, name="n"):
         self.quant = quant
         self.e = e
         self.disp = disp
+        self.name = name
