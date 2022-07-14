e19f33563bf76c2e29f356fe9025561c9dd09326
TitouanCH <titouan.azert@wadsa.com>
2022-07-14 22:53:25 +0200

    removing everything before big push

--- molecule.py
+++
@@ -1,8 +0,0 @@
-#CLASS
-
-class Molecule :
-    def __init__(self, e=0, quant=0, disp=0, name="n"):
-        self.quant = quant
-        self.e = e
-        self.disp = disp
-        self.name = name
